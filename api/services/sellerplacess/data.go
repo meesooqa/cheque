44ab2823dfc9c9c6704c0547713e490a5cb47530
meesooqa <meesooqa@hotmail.com>
2025-03-10 11:35:54 +0300

    api - sellerplaces - data, filters

--- api/services/sellerplacess/data.go
+++ api/services/sellerplacess/data.go
@@ -10,10 +10,19 @@ func NewConverter() *Converter {
 
 func (o *Converter) DataDbToPb(dbItem *DbModel) *pb.Model {
 	return &pb.Model{
-		Id: uint64(dbItem.ID),
+		Id:       uint64(dbItem.ID),
+		SellerID: uint64(dbItem.SellerID),
+		Name:     dbItem.Name,
+		Address:  dbItem.Address,
+		Email:    dbItem.Email,
 	}
 }
 
 func (o *Converter) DataPbToDb(pbItem *pb.Model) *DbModel {
-	return &DbModel{}
+	return &DbModel{
+		SellerID: uint(pbItem.SellerID),
+		Name:     pbItem.Name,
+		Address:  pbItem.Address,
+		Email:    pbItem.Email,
+	}
 }
--- api/services/sellerplacess/filters.go
+++ api/services/sellerplacess/filters.go
@@ -6,8 +6,38 @@ import (
 	"cheque-04/api/services"
 )
 
-func ExampleFilter(value string) services.FilterFunc {
+func SellerIDFilter(value uint64) services.FilterFunc {
 	return func(db *gorm.DB) *gorm.DB {
+		if value != 0 {
+			return db.Where("seller_id=?", value)
+		}
+		return db
+	}
+}
+
+func NameFilter(value string) services.FilterFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		if value != "" {
+			return db.Where("value ILIKE ?", "%"+value+"%")
+		}
+		return db
+	}
+}
+
+func AddressFilter(value string) services.FilterFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		if value != "" {
+			return db.Where("inn ILIKE ?", "%"+value+"%")
+		}
+		return db
+	}
+}
+
+func EmailFilter(value string) services.FilterFunc {
+	return func(db *gorm.DB) *gorm.DB {
+		if value != "" {
+			return db.Where("inn ILIKE ?", "%"+value+"%")
+		}
 		return db
 	}
 }
