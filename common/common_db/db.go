e9f605b135af39c6ef579da980e92da25faf2e5a
meesooqa <meesooqa@hotmail.com>
2025-03-11 08:03:16 +0300

    common - common_db.GetDB

---
+++ common/common_db/db.go
@@ -0,0 +1,25 @@
+package common_db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/meesooqa/cheque/common/config"
+)
+
+func GetDB() *gorm.DB {
+	conf, err := config.GetConf()
+	if err != nil {
+		log.Fatalf("can't load config: %v", err)
+	}
+	dsn := fmt.Sprintf("host=%s port=%d sslmode=%s user=%s password=%s dbname=%s", conf.DB.Host, conf.DB.Port, conf.DB.SslMode, conf.DB.User, conf.DB.Password, conf.DB.DbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+	return db.WithContext(context.TODO())
+}
