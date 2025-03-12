41f755aa18aed6554ac621fff9e66f8f55189d8c
meesooqa <meesooqa@hotmail.com>
2025-03-12 19:22:30 +0300

    api - refactor BaseService (log -> Logger)

--- api/services/service.go
+++ api/services/service.go
@@ -14,14 +14,14 @@ type Converter[DbModel any, PbModel any] interface {
 type FilterFunc func(db *gorm.DB) *gorm.DB
 
 type BaseService[DbModel any, PbModel any] struct {
-	log       *slog.Logger
+	Logger    *slog.Logger
 	db        *gorm.DB
 	converter Converter[DbModel, PbModel]
 }
 
 func NewBaseService[T any, U any](log *slog.Logger, db *gorm.DB, converter Converter[T, U]) *BaseService[T, U] {
 	return &BaseService[T, U]{
-		log:       log,
+		Logger:    log,
 		db:        db,
 		converter: converter,
 	}
