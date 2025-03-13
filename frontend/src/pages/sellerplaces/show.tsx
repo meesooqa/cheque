a5a5a71fcbbbc5cf394bafc54f8be359187e9bf9
meesooqa <meesooqa@hotmail.com>
2025-03-13 15:41:18 +0300

    frontend - sellerplaces - show

--- frontend/src/pages/sellerplaces/show.tsx
+++ frontend/src/pages/sellerplaces/show.tsx
@@ -10,8 +10,16 @@ export const SellerplacesShow = () => {
     const record = data?.data;
     return (
         <Show isLoading={isLoading}>
+            <Title level={5}>{"id"}</Title>
+            <TextField value={record?.id} />
+            <Title level={5}>{"sellerID"}</Title>
+            <TextField value={record?.sellerID} />
             <Title level={5}>{"name"}</Title>
             <TextField value={record?.name} />
+            <Title level={5}>{"address"}</Title>
+            <TextField value={record?.address} />
+            <Title level={5}>{"email"}</Title>
+            <TextField value={record?.email} />
         </Show>
     );
 };
