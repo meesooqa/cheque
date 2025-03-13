e646be47c0cd9f9d02c4854970c3d2027d3327f6
meesooqa <meesooqa@hotmail.com>
2025-03-13 15:47:10 +0300

    frontend - sellerplaces - create

--- frontend/src/pages/sellerplaces/create.tsx
+++ frontend/src/pages/sellerplaces/create.tsx
@@ -6,7 +6,10 @@ export const SellerplacesCreate = () => {
     return (
         <Create saveButtonProps={saveButtonProps}>
             <Form {...formProps} layout="vertical">
+                <Form.Item label={"sellerID"} name={["sellerID"]} rules={[{required: true}]}><Input /></Form.Item>
                 <Form.Item label={"name"} name={["name"]} rules={[{required: true}]}><Input /></Form.Item>
+                <Form.Item label={"address"} name={["address"]} rules={[{required: true}]}><Input /></Form.Item>
+                <Form.Item label={"email"} name={["email"]}><Input type="email" /></Form.Item>
             </Form>
         </Create>
     );
