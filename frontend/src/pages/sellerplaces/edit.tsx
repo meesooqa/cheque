20fcaaeef4a8c02c14ab535b6654f97be942cccd
meesooqa <meesooqa@hotmail.com>
2025-03-13 15:46:24 +0300

    frontend - sellerplaces - edit

--- frontend/src/pages/sellerplaces/edit.tsx
+++ frontend/src/pages/sellerplaces/edit.tsx
@@ -1,4 +1,4 @@
-import { Edit, useForm } from "@refinedev/antd";
+import {Edit, useForm} from "@refinedev/antd";
 import { Form, Input } from "antd";
 
 export const SellerplacesEdit = () => {
@@ -6,7 +6,10 @@ export const SellerplacesEdit = () => {
     return (
         <Edit saveButtonProps={saveButtonProps}>
             <Form {...formProps} layout="vertical">
+                <Form.Item label={"sellerID"} name={["sellerID"]} rules={[{required: true}]}><Input /></Form.Item>
                 <Form.Item label={"name"} name={["name"]} rules={[{required: true}]}><Input /></Form.Item>
+                <Form.Item label={"address"} name={["address"]} rules={[{required: true}]}><Input /></Form.Item>
+                <Form.Item label={"email"} name={["email"]}><Input type="email" /></Form.Item>
             </Form>
         </Edit>
     );
