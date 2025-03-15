744e86583b02971669f15ec06b01376733783423
meesooqa <meesooqa@hotmail.com>
2025-03-15 12:00:03 +0300

    api - refactor filters - sellerplaces

--- api/services/sellerplacess/filters.go
+++ api/services/sellerplacess/filters.go
@@ -3,8 +3,18 @@ package sellerplacess
 import (
 	"gorm.io/gorm"
 
+	pb "github.com/meesooqa/cheque/api/pb/sellerplacepb"
 	"github.com/meesooqa/cheque/api/services"
 )
+
+func GetFilters(req *pb.GetListRequest) []services.FilterFunc {
+	return []services.FilterFunc{
+		SellerIDFilter(req.SellerId),
+		NameFilter(req.Name),
+		AddressFilter(req.Address),
+		EmailFilter(req.Email),
+	}
+}
 
 func SellerIDFilter(value uint64) services.FilterFunc {
 	return func(db *gorm.DB) *gorm.DB {
