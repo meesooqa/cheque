0cbbfee8ee68a1d4086e83f894fcf4c1b48af2cf
meesooqa <meesooqa@hotmail.com>
2025-03-18 17:47:50 +0300

    api-server - log request body in middleware

--- api-server/web/middlewares/log.go
+++ api-server/web/middlewares/log.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"bytes"
+	"io"
 	"log/slog"
 	"net/http"
 )
@@ -17,7 +19,20 @@ func NewLog(logger *slog.Logger) *Log {
 
 func (o *Log) Handle(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		o.logger.Info("received request", slog.String("method", r.Method), slog.String("URL.Path", r.URL.Path))
+		o.logger.Info("received request",
+			slog.String("method", r.Method),
+			slog.String("URL.Path", r.URL.Path))
+		// body
+		if r.Method == "PATCH" || r.Method == "POST" || r.Method == "PUT" {
+			bodyBytes, err := io.ReadAll(r.Body)
+			if err != nil {
+				o.logger.Error("failed to read request body", slog.String("error", err.Error()))
+			} else {
+				// Восстанавливаем тело запроса для последующей обработки
+				r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+				o.logger.Info("request body", slog.String("body", string(bodyBytes)))
+			}
+		}
 		next.ServeHTTP(w, r)
 	})
 }
