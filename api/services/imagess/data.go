f35e13738f812c43da712b7341e5f76bec31e1b3
meesooqa <meesooqa@hotmail.com>
2025-03-19 11:26:31 +0300

    api - images - url

--- api/services/imagess/data.go
+++ api/services/imagess/data.go
@@ -1,19 +1,33 @@
 package imagess
 
-import pb "github.com/meesooqa/cheque/api/pb/imagepb"
+import (
+	"fmt"
+	"log"
 
-type Converter struct{}
+	pb "github.com/meesooqa/cheque/api/pb/imagepb"
+	"github.com/meesooqa/cheque/common/config"
+)
+
+type Converter struct {
+	conf *config.Conf
+}
 
 func NewConverter() *Converter {
-	return &Converter{}
+	conf, err := config.GetConf()
+	if err != nil {
+		log.Fatalf("can't load config: %v", err)
+	}
+	return &Converter{conf: conf}
 }
 
 func (o *Converter) DataDbToPb(dbItem *DbModel) *pb.Model {
+	// TODO http://localhost:5173/var/upload/6/P1482164.jpg
+	url := fmt.Sprintf("/%s/%d/%s", o.conf.System.UploadPath, dbItem.ProductID, dbItem.URL)
 	return &pb.Model{
 		Id:        uint64(dbItem.ID),
 		ProductId: uint64(dbItem.ProductID),
 		Name:      dbItem.Name,
-		Url:       dbItem.URL,
+		Url:       url,
 		Order:     int32(dbItem.Order),
 		IsMain:    dbItem.IsMain,
 	}
