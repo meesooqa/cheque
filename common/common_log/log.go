e3fb5d02fd6a3628a7d0d2474606af0dcffe6ae3
meesooqa <meesooqa@hotmail.com>
2025-03-20 21:15:07 +0300

    common - refactor log

--- common/common_log/log.go
+++ common/common_log/log.go
@@ -6,25 +6,53 @@ import (
 	"os"
 )
 
-// InitConsoleLogger returns logger to write to Stdout
-func InitConsoleLogger(level slog.Level) *slog.Logger {
-	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		Level: level,
-	}))
+type LoggerProvider interface {
+	GetLogger() (*slog.Logger, func())
 }
 
-// InitFileLogger returns loggerFile and cleanup func.
-// logger, cleanup := config.InitLogger("var/log/server.log", slog.LevelDebug)
+type ConsoleLoggerProvider struct {
+	level slog.Level
+}
+
+func NewConsoleLoggerProvider(level slog.Level) *ConsoleLoggerProvider {
+	return &ConsoleLoggerProvider{
+		level: level,
+	}
+}
+
+// GetLogger returns logger writing to Stdout
+func (o *ConsoleLoggerProvider) GetLogger() (*slog.Logger, func()) {
+	noop := func() {
+		// skip any actions
+	}
+	return slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: o.level,
+	})), noop
+}
+
+type FileLoggerProvider struct {
+	level    slog.Level
+	filePath string // "var/log/server.log"
+}
+
+func NewFileLoggerProvider(level slog.Level, filePath string) *FileLoggerProvider {
+	return &FileLoggerProvider{
+		level:    level,
+		filePath: filePath,
+	}
+}
+
+// GetLogger returns logger writing to file and cleanup func.
+// logger, cleanup := loggerProvider.GetLogger("var/log/server.log", slog.LevelDebug)
 // defer cleanup()
-func InitFileLogger(logFilePath string, level slog.Level) (*slog.Logger, func()) {
-	file, err := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+func (o *FileLoggerProvider) GetLogger() (*slog.Logger, func()) {
+	file, err := os.OpenFile(o.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		log.Fatalf("failed to open log file: %v", err)
 	}
-	handler := slog.NewTextHandler(file, &slog.HandlerOptions{
-		Level: level,
-	})
-	logger := slog.New(handler)
+	logger := slog.New(slog.NewTextHandler(file, &slog.HandlerOptions{
+		Level: o.level,
+	}))
 	cleanup := func() {
 		file.Close()
 	}
