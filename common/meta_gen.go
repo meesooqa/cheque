d5216b07ec415dc3f3c5569e18eaa9bbfcb30ca6
meesooqa <meesooqa@hotmail.com>
2025-03-20 20:24:07 +0300

    common - TODO RegisterGormData()

--- common/meta_gen.go
+++ common/meta_gen.go
@@ -1,7 +1,7 @@
 package common
 
 import (
-	"github.com/meesooqa/cheque/common/models"
+	"github.com/meesooqa/cheque/db/models"
 	"github.com/meesooqa/cheque/gorm-gen-proto/gen"
 	"github.com/meesooqa/cheque/gorm-gen-proto/reg"
 )
@@ -14,15 +14,15 @@ func init() {
 		PkgProtobuf      string
 		PkgServiceServer string
 	}{
-		// TODO &models.Seller{}
-		{models.Seller{}, "models.Seller", "sellers", "sellerpb", "sellerss"},
-		{models.SellerPlace{}, "models.SellerPlace", "sellerplaces", "sellerplacepb", "sellerplacess"},
-		{models.Category{}, "models.Category", "categories", "categorypb", "categoryss"},
-		{models.Brand{}, "models.Brand", "brands", "brandpb", "brandss"},
-		{models.Product{}, "models.Product", "products", "productpb", "productss"},
-		{models.Image{}, "models.Image", "images", "imagepb", "imagess"},
-		{models.Receipt{}, "models.Receipt", "receipts", "receiptpb", "receiptss"},
-		{models.ReceiptProduct{}, "models.ReceiptProduct", "receiptsproducts", "receiptproductpb", "receiptproductss"},
+		// TODO move to db
+		{&models.Seller{}, "models.Seller", "sellers", "sellerpb", "sellerss"},
+		{&models.SellerPlace{}, "models.SellerPlace", "sellerplaces", "sellerplacepb", "sellerplacess"},
+		{&models.Category{}, "models.Category", "categories", "categorypb", "categoryss"},
+		{&models.Brand{}, "models.Brand", "brands", "brandpb", "brandss"},
+		{&models.Product{}, "models.Product", "products", "productpb", "productss"},
+		{&models.Image{}, "models.Image", "images", "imagepb", "imagess"},
+		{&models.Receipt{}, "models.Receipt", "receipts", "receiptpb", "receiptss"},
+		{&models.ReceiptProduct{}, "models.ReceiptProduct", "receiptsproducts", "receiptproductpb", "receiptproductss"},
 	}
 
 	var gormData []*gen.GormForTmpl
@@ -33,7 +33,7 @@ func init() {
 
 	var ssData []*gen.SsTmplData
 	ImportPbPrefix := "github.com/meesooqa/cheque/api/pb"
-	importModels := "github.com/meesooqa/cheque/common/models"
+	importModels := "github.com/meesooqa/cheque/db/models"
 	importServices := "github.com/meesooqa/cheque/api/services"
 	for _, d := range data {
 		ssData = append(ssData, &gen.SsTmplData{Package: d.PkgServiceServer, DbModel: d.ModelDeclaration, ImportPb: ImportPbPrefix + "/" + d.PkgProtobuf, ImportServices: importServices, ImportModels: importModels})
