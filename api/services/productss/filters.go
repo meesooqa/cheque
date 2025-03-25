23f39d526d04d44f5ee2faee2759e8c94bfd9613
meesooqa <meesooqa@hotmail.com>
2025-03-25 21:51:13 +0300

    api - productss - use typed filters

--- api/services/productss/filters.go
+++ api/services/productss/filters.go
@@ -1,33 +1,15 @@
 package productss
 
 import (
-	"gorm.io/gorm"
-
 	pb "github.com/meesooqa/cheque/api/gen/pb/productpb/v1"
 	"github.com/meesooqa/cheque/db/db_types"
 )
 
 func GetFilters(req *pb.GetListRequest) []db_types.FilterFunc {
+	BrandIDFilter := db_types.ModelExactFieldFilter[DbModel]("brand_id")
+	NameFilter := db_types.ModelFieldFilter[DbModel]("name")
 	return []db_types.FilterFunc{
 		BrandIDFilter(req.BrandId),
 		NameFilter(req.Name),
-	}
-}
-
-func BrandIDFilter(value uint64) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != 0 {
-			return db.Where("brand_id = ?", value)
-		}
-		return db
-	}
-}
-
-func NameFilter(value string) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != "" {
-			return db.Where("name ILIKE ?", "%"+value+"%")
-		}
-		return db
 	}
 }
