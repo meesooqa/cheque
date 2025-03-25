55fec873556b5680c85c5940808ac8fbada12424
meesooqa <meesooqa@hotmail.com>
2025-03-25 21:45:03 +0300

    api - categoryss - use typed filters

--- api/services/categoryss/filters.go
+++ api/services/categoryss/filters.go
@@ -1,33 +1,15 @@
 package categoryss
 
 import (
-	"gorm.io/gorm"
-
 	pb "github.com/meesooqa/cheque/api/gen/pb/categorypb/v1"
 	"github.com/meesooqa/cheque/db/db_types"
 )
 
 func GetFilters(req *pb.GetListRequest) []db_types.FilterFunc {
+	ParentIDFilter := db_types.ModelExactFieldFilter[DbModel]("parent_id")
+	NameFilter := db_types.ModelFieldFilter[DbModel]("name")
 	return []db_types.FilterFunc{
 		NameFilter(req.Name),
 		ParentIDFilter(req.ParentId),
-	}
-}
-
-func NameFilter(value string) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != "" {
-			return db.Where("name ILIKE ?", "%"+value+"%")
-		}
-		return db
-	}
-}
-
-func ParentIDFilter(value uint64) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != 0 {
-			return db.Where("parent_id = ?", value)
-		}
-		return db
 	}
 }
