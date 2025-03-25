e1db4be18dfc6917b51b916335cf5800f0e7c87d
meesooqa <meesooqa@hotmail.com>
2025-03-25 22:05:09 +0300

    db, api - receiptproductss - add ModelRangeFilter and use typed filters

--- api/services/receiptproductss/filters.go
+++ api/services/receiptproductss/filters.go
@@ -1,75 +1,21 @@
 package receiptproductss
 
 import (
-	"gorm.io/gorm"
-
 	pb "github.com/meesooqa/cheque/api/gen/pb/receiptproductpb/v1"
 	"github.com/meesooqa/cheque/db/db_types"
 )
 
 func GetFilters(req *pb.GetListRequest) []db_types.FilterFunc {
+	ProductIDFilter := db_types.ModelExactFieldFilter[DbModel]("product_id")
+	ReceiptIDFilter := db_types.ModelExactFieldFilter[DbModel]("receipt_id")
+	PriceFilter := db_types.ModelRangeFilter[DbModel, int32]("price")
+	SumFilter := db_types.ModelRangeFilter[DbModel, int32]("sum")
+	QuantityFilter := db_types.ModelRangeFilter[DbModel, float64]("quantity")
 	return []db_types.FilterFunc{
 		ProductIDFilter(req.ProductId),
 		ReceiptIDFilter(req.ReceiptId),
 		PriceFilter(req.PriceGt, req.PriceLt),
 		SumFilter(req.SumGt, req.SumLt),
 		QuantityFilter(req.QuantityGt, req.QuantityLt),
-	}
-}
-
-func ProductIDFilter(value uint64) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != 0 {
-			return db.Where("product_id = ?", value)
-		}
-		return db
-	}
-}
-
-func ReceiptIDFilter(value uint64) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if value != 0 {
-			return db.Where("receipt_id = ?", value)
-		}
-		return db
-	}
-}
-
-func PriceFilter(valueGt, valueLt int32) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if valueGt > 0 && valueLt > 0 {
-			db = db.Where("price BETWEEN ? AND ?", valueGt, valueLt)
-		} else if valueGt > 0 {
-			db = db.Where("price >= ?", valueGt)
-		} else if valueLt > 0 {
-			db = db.Where("price <= ?", valueLt)
-		}
-		return db
-	}
-}
-
-func SumFilter(valueGt, valueLt int32) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if valueGt > 0 && valueLt > 0 {
-			db = db.Where("sum BETWEEN ? AND ?", valueGt, valueLt)
-		} else if valueGt > 0 {
-			db = db.Where("sum >= ?", valueGt)
-		} else if valueLt > 0 {
-			db = db.Where("sum <= ?", valueLt)
-		}
-		return db
-	}
-}
-
-func QuantityFilter(valueGt, valueLt float64) db_types.FilterFunc {
-	return func(db *gorm.DB) *gorm.DB {
-		if valueGt > 0 && valueLt > 0 {
-			db = db.Where("quantity BETWEEN ? AND ?", valueGt, valueLt)
-		} else if valueGt > 0 {
-			db = db.Where("quantity >= ?", valueGt)
-		} else if valueLt > 0 {
-			db = db.Where("quantity <= ?", valueLt)
-		}
-		return db
 	}
 }
