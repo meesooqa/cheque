96430a88484a381799a9e53fc63b71eb4efa1a60
meesooqa <meesooqa@hotmail.com>
2025-03-26 10:40:24 +0300

    api - set up generator

--- api/services/ssgen.go
+++ api/services/ssgen.go
@@ -1,5 +1,21 @@
-//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template service
-//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template methods
-//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template data
-//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template filters
+//go:generate0 go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template service
+//go:generate0 go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template methods
+//go:generate0 go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template data
+//go:generate0 go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template filters
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Brand -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Brand -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Category -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Category -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Image -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Image -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Product -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Product -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity ReceiptProduct -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity ReceiptProduct -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Receipt -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Receipt -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity SellerPlace -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity SellerPlace -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Seller -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Seller -template methods
 package services
