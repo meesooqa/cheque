919d3b036c4d18fc14d70aeda5a60a6fa4c84b2d
meesooqa <meesooqa@hotmail.com>
2025-03-26 09:34:49 +0300

    api - add FilterProvider

--- api/services/service.go
+++ api/services/service.go
@@ -2,19 +2,29 @@ package services
 
 import "github.com/meesooqa/cheque/db/db_types"
 
-type Converter[DbModel any, PbModel any] interface {
+type Converter[DbModel, PbModel any] interface {
 	DataDbToPb(dbItem *DbModel) *PbModel
 	DataPbToDb(pbItem *PbModel) *DbModel
 }
 
-type BaseService[DbModel any, PbModel any] struct {
-	Repo      db_types.Repository[DbModel]
-	Converter Converter[DbModel, PbModel]
+type FilterProvider[PbGetListRequest any] interface {
+	GetFilters(r *PbGetListRequest) []db_types.FilterFunc
 }
 
-func NewBaseService[DbModel any, PbModel any](repo db_types.Repository[DbModel], converter Converter[DbModel, PbModel]) *BaseService[DbModel, PbModel] {
-	return &BaseService[DbModel, PbModel]{
-		Repo:      repo,
-		Converter: converter,
+type BaseService[DbModel, PbModel, PbGetListRequest any] struct {
+	Repo           db_types.Repository[DbModel]
+	Converter      Converter[DbModel, PbModel]
+	FilterProvider FilterProvider[PbGetListRequest]
+}
+
+func NewBaseService[DbModel, PbModel, PbGetListRequest any](
+	repo db_types.Repository[DbModel],
+	converter Converter[DbModel, PbModel],
+	filterProvider FilterProvider[PbGetListRequest],
+) *BaseService[DbModel, PbModel, PbGetListRequest] {
+	return &BaseService[DbModel, PbModel, PbGetListRequest]{
+		Repo:           repo,
+		Converter:      converter,
+		FilterProvider: filterProvider,
 	}
 }
