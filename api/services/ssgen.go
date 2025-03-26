dfc52347b998dffe402c0ade1281a638aef1bab4
meesooqa <meesooqa@hotmail.com>
2025-03-26 10:30:00 +0300

    api - add test generator annotation

---
+++ api/services/ssgen.go
@@ -0,0 +1,5 @@
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template service
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template methods
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template data
+//go:generate go run github.com/meesooqa/cheque/tools/api/cmd/ssgen -entity Entity -template filters
+package services
